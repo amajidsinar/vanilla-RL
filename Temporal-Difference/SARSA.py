c81f38ab83a14cc122ff3ec122717e1806eff27e
Amajid Sinar <amajidsinar@gmail.com>
2018-07-05 11:12:01 +0700

    change the structure of the code from generating the whole episode and count the Q to directly compute Q for each time step

--- Temporal-Difference/SARSA.py
+++ Temporal-Difference/SARSA.py
@@ -18,11 +18,11 @@ import random
 
 env = gym.make('FrozenLake-v0')
 
-episodes = 100
+episodes = 100000
 timesteps = 100
 
-diamond = 20
-pit = -20
+diamond = 100
+pit = -15
 cost = -1
 
 nA = env.action_space.n
@@ -32,13 +32,15 @@ Q = defaultdict(lambda:np.zeros(nA))
 alpha = 0.9
 gamma = 0.9
 
-
+#define the soft policy function
 def soft_policy(Q,epsilon=0.3):
+    #create the probability distribution
     q = np.ones(nA) * epsilon / nA
     q[np.argmax(Q)] += 1 - epsilon
     
     roll_the_dice = random.random()
     
+    #self explanatory
     cumsum = []
     for i,val in enumerate(q):
         cumsum.append(sum([j for j in q[:i+1]]))
@@ -49,19 +51,24 @@ def soft_policy(Q,epsilon=0.3):
 
 for e in range(episodes):
     state = env.reset()
-    episode = []
     for t in range(timesteps):
-        action = soft_policy(Q[state])
+        #take action a to land state s'
+        action = soft_policy(Q[state],0.3)
         next_state,reward,done,_ = env.step(action)
+        #set different reward based on the next state
+        if next_state == 15:
+            reward = diamond
+        elif next_state==5 or next_state==7 or next_state==11 or next_state==12:
+            reward = pit
+        else:
+            reward = cost
+        #from state s' pick the a' but dont take the move yet
+        next_action = soft_policy(Q[next_state],0.3)
+        #estimate the current value by sampling the next step or how much we would get if we take action a'
+        #in the next state
+        sample = gamma * Q[next_state][next_action]
+        Q[state][action] += alpha * (reward + sample - Q[state][action])
         if done:
-            if next_state == 15:
-                episode.append((state,action,next_state,diamond,done))
-            else:
-                episode.append((state,action,next_state,pit,done))
-            break
-        episode.append((state,action,next_state,cost,done))
+            break;
         state = next_state
-    for i,val in zip(range(len(episode)-1),episode):
-        sample = gamma * Q[episode[i+1][0]][episode[i+1][1]]
-        Q[val[0]][val[1]] += alpha * (val[3] + sample - Q[val[0]][val[1]]) 
         
