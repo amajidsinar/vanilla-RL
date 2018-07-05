0ae440054a9f82c8d30bb4f63c8aea9831687253
Amajid Sinar <amajidsinar@gmail.com>
2018-07-05 11:33:46 +0700

    change the structure of the code from generating the whole episode and count the Q to directly compute Q for each time step

--- Temporal-Difference/TD Prediction.py
+++ Temporal-Difference/TD Prediction.py
@@ -28,17 +28,19 @@ V = defaultdict(float)
 
 for e in range(episodes):
     state = env.reset()
-    episode = []
     for t in range(timesteps):
         action = env.action_space.sample()
         next_state,reward,done,_ = env.step(action)
+        #set different reward based on the next state
+        if next_state == 15:
+            reward = diamond
+        elif next_state==5 or next_state==7 or next_state==11 or next_state==12:
+            reward = pit
+        else:
+            reward = cost
+        #take the V in the next step as the prediction
+        sample = gamma * V[next_state]
+        V[state] += alpha * (reward + sample - V[state])
         if done:
-            if next_state == 15:
-                episode.append((state,next_state,diamond,done))
-            else:
-                episode.append((state,next_state,pit,done))
             break
-        episode.append((state,next_state,cost,done))
         state = next_state
-    for val in episode:
-        V[val[0]] += alpha * (val[2] + (gamma*(V[val[1]])) - V[val[0]])
